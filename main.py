e932cf2ae2b29cde0200fa3299fb1cca01b25daf
Mohamed Magdy <109232133+mohameedmagdii@users.noreply.github.com>
2024-11-10 14:31:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class FotmobAPI:
         ]
         user_agent = random.choice(uastrings)
         headers = {'User-Agent': user_agent,
-                  "x-fm-req": "eyJib2R5Ijp7InVybCI6Ii9hcGkvbWF0Y2hEZXRhaWxzP21hdGNoSWQ9NDUwNjQ3MyIsImNvZGUiOjE3MzEyMzQ0MTU1NzIsImZvbyI6IjgxNTc1NWYwMiJ9LCJzaWduYXR1cmUiOiI3NTE5QUVFNTdEMTI3RkFENDQ3QjYxRjk5MjQ1RDA4NiJ9"}
+                  "x-fm-req": requests.get('http://46.101.91.154:6006/').json()['x-fm-req']}
         response = requests.get(link, headers=headers)
         response.raise_for_status()
         response_json = response.json()
